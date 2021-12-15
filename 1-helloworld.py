3ae28758afe7c901685a2c0680ec5fdaa00f4f63
Kieran White <96084718+kieranwhite82@users.noreply.github.com>
2021-12-15 12:21:43 +0000

    Add files via upload

---
+++ 1-helloworld.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 16:11:11 2021
+
+@author: uqkwhi18
+"""
+
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+
+# Main program starts here
+hello()
